dca56695c2a57bf89b48ec488b1efcd282a70395
vanchBim <stecenkoivan1995@gmail.com>
2022-03-30 22:43:02 +0300

    add lesson 5

---
+++ Lesson5/MetricsAgent/Job/CpuMetricsJob.cs
@@ -0,0 +1,37 @@
+﻿using MetricsAgent.DAL;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+namespace MetricsAgent.Jobs
+{
+    public class CpuMetricJob : IJob
+    {
+        private ICpuMetricsRepository _repository;
+        // Счётчик для метрики CPU
+        private PerformanceCounter _cpuCounter;
+        public CpuMetricJob(ICpuMetricsRepository repository)
+        {
+            _repository = repository;
+            _cpuCounter = new PerformanceCounter ("Processor", "% Processor Time", "_Total");
+        }
+
+public Task Execute(IJobExecutionContext context)
+        {
+            // Получаем значение занятости CPU
+            var cpuUsageInPercents = Convert.ToInt32(_cpuCounter.NextValue());
+            // Узнаем, когда мы сняли значение метрики
+            var time =
+            TimeSpan.FromSeconds(DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+            // Теперь можно записать что-то посредством репозитория
+            _repository.Create(new CpuMetric
+            {
+                Time = time,
+                Value =
+            cpuUsageInPercents
+            });
+            return Task.CompletedTask;
+        }
+    }
+}
